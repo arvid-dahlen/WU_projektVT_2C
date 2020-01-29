17651173035010644d7860023563c5ec702e7756
Arvid Dahlén <arvid.dahlen@elev.ga.ntig.se>
2020-01-29 21:05:55 +0100

    *Ett par mindre förändringar i skriptet*

--- js/script.js
+++ js/script.js
@@ -25,16 +25,44 @@ const Slide = ()=>{
 
 const Share = ()=>{
     const shareButton = document.querySelector('.center-box');
-    
+    const shareButton1 = document.querySelector('.b1');
+    const shareButton2 = document.querySelector('.b2');
+    const shareButton3 = document.querySelector('.b3');
+    const shareButton4 = document.querySelector('.b4');
+    const shareButton6 = document.querySelector('.b6');
+    const shareButton7 = document.querySelector('.b7');
+    const shareButton8 = document.querySelector('.b8');
+    const shareButton9 = document.querySelector('.b9');
+
     shareButton.addEventListener('click',() => {
         shareButton.classList.toggle('center-box-active');
+        shareButton1.classList.toggle('box-active');
+        shareButton2.classList.toggle('box-active');
+        shareButton3.classList.toggle('box-active');
+        shareButton4.classList.toggle('box-active');
+        shareButton6.classList.toggle('box-active');
+        shareButton7.classList.toggle('box-active');
+        shareButton8.classList.toggle('box-active');
+        shareButton9.classList.toggle('box-active');
     });
+        
+    }
+
+const changeIcon = ()=>{ 
+    const shareButton = document.querySelector('.center-box');
+
+    shareButton.addEventListener('click',() => {
+         if(document.querySelector("#icon").className == 'fa fa-share-alt'){
+            document.querySelector("#icon").className = "fa fa-times";
+
+        }else{   
+            document.querySelector("#icon").className = "fa fa-share-alt";
+        }
+    });
+
 }
 
-function changeIcon(x) {
-    x.classList.toggle("fa-instagram");
-}
-
+changeIcon();
 Share();
 Slide();    
 
